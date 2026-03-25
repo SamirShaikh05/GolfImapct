443622e4c68e8320c44fdcc3eaf97781052cf335
Samir Shaikh <samirshaikh050505@gmail.com>
2026-03-26 00:56:24 +0530

    adding port in env

--- backend/server.js
+++ backend/server.js
@@ -16,4 +16,9 @@ app.use("/api/user", require("./routes/user"));
 app.use("/api/admin", require("./routes/admin"));
 app.use("/api/payment", require("./routes/payment"));
 
-app.listen(5000, ()=>console.log("Server running"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
