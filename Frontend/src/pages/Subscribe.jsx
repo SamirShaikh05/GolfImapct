a731df7c7776995aba08f36d387d2f2ae618e2d0
Samir Shaikh <samirshaikh050505@gmail.com>
2026-03-26 01:25:11 +0530

    adding env in frontend

--- Frontend/src/pages/Subscribe.jsx
+++ Frontend/src/pages/Subscribe.jsx
@@ -1,21 +1,26 @@
+const API = import.meta.env.VITE_API_URL;
+
 export default function Subscribe() {
   const pay = async () => {
-  const res = await fetch("http://localhost:5000/api/payment/checkout", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": localStorage.getItem("token")
-    }
-  });
+    const res = await fetch(`${API}/payment/checkout`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": localStorage.getItem("token")
+      }
+    });
 
-  const data = await res.json();
-  window.location.href = data.url;
-};
+    const data = await res.json();
+    window.location.href = data.url;
+  };
 
   return (
     <div className="p-10">
       <h2 className="text-2xl mb-4">Subscribe</h2>
-      <button onClick={pay} className="bg-green-500 text-white px-6 py-2">
+      <button
+        onClick={pay}
+        className="bg-green-500 text-white px-6 py-2"
+      >
         Subscribe Now
       </button>
     </div>
